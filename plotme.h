c07181fce3360e031ce9e37941209c94f6a7a781
omkarjunnarkar <93515440+omkarjunnarkar@users.noreply.github.com>
2022-02-09 21:42:01 +0100

    Add files via upload

---
+++ plotme.h
@@ -0,0 +1,12 @@
+/*
+***********************************************************************************************************************************************************
+
+												Finite Element Code for Tapered Bar Problem
+														Header File Plotting Routine
+
+Author: Omkar Junnarkar, Semester-3 MSc. Computational Material Science
+Matriculation Nr.: 66157	Email: omkar.junnarkar@student.tu-freiberg.de
+IDE : Microsoft Visual Studio 2019
+*/
+
+int plot();
